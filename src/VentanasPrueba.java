197d09ee985faa72790297b4308008eef5632bda
Beto Jr <Beto Jr@192.168.19.9>
2020-10-07 22:09:00 -0500

    1.1

---
+++ src/VentanasPrueba.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Beto Jr
+ */
+import javax.swing.*;
+
+public class VentanasPrueba {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Mi Ventana");
+        f.setSize(400, 300);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
